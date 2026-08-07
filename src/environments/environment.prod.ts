413ccaa8045060c5ce54ade86077d6c9da6b4593
BagZzi <igor.bakovic4@gmail.com>
2026-08-07 16:20:13 +0200

    Konfigurisani environment i HttpClientModule

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseApiKey: 'AIzaSyAwm5QLw6Kz751do4htQ0e67zBVF5r57kA',
+  firebaseDatabaseUrl: 'https://pozajmica-app-default-rtdb.europe-west1.firebasedatabase.app',
+  firebaseAuthUrl: 'https://identitytoolkit.googleapis.com/v1/accounts'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseApiKey: 'AIzaSyAwm5QLw6Kz751do4htQ0e67zBVF5r57kA',
+  firebaseDatabaseUrl: 'https://pozajmica-app-default-rtdb.europe-west1.firebasedatabase.app',
+  firebaseAuthUrl: 'https://identitytoolkit.googleapis.com/v1/accounts'
 };
 
 /*
